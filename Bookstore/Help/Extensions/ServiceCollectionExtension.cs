10c052ee1c9c1d26a1ee61f13eb35281cb043e88
= <=>
2022-08-12 08:52:10 -0300

    Creating inmemorydb and structure

---
+++ Bookstore/Help/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Bookstore.Infrastructure.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookstore.Help.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<DbContext, BookstoreContext>();
+
+            return serviceCollection;
+        }
+    }
+}
---
+++ Bookstore/Infrastructure/Base/BookstoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Bookstore.Help.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Infrastructure.Base
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> opt) : base(opt) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new BookMapping());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
